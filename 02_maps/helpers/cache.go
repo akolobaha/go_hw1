4f53b35cfefa8dd663e8a16a6202ddc28ef88b78
Rustam Khannanov <sk-dev@yandex.ru>
2024-05-26 22:57:02 +0300

    feat: задача 4 refs:02_maps

---
+++ 02_maps/helpers/cache.go
@@ -0,0 +1,10 @@
+package helpers
+
+type Cache[K comparable, V any] struct{ m map[K]V }
+
+func (c Cache[K, V]) Init()               { c.m = make(map[K]V) }
+func (c *Cache[K, V]) Set(key K, value V) { c.m[key] = value }
+func (c *Cache[K, V]) Get(key K) (V, bool) {
+	k, ok := c.m[key]
+	return k, ok
+}
