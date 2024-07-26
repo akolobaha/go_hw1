3469658dbd4c2ec2cf4cc4042bc373cedf1f1518
Rustam Khannanov <sk-dev@yandex.ru>
2024-07-26 14:36:24 +0300

    feat: rateLimiter final

--- 2_1_rate_limiter/client/main.go
+++ 2_1_rate_limiter/client/main.go
@@ -8,10 +8,7 @@ import (
 )
 
 var url = "localhost:8080"
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
+var requests = 30
 
 func main() {
 	//go server()
@@ -20,17 +17,8 @@ func main() {
 
 }
 
-//func server() {
-//	http.HandleFunc("/", handler)
-//
-//	fmt.Println("Сервер запущен на http://localhost:8080/")
-//	if err := http.ListenAndServe(url, nil); err != nil {
-//		fmt.Println("Ошибка при запуске сервера:", err)
-//	}
-//}
-
 func client(wg *sync.WaitGroup) {
-	for range 10 {
+	for range requests {
 		go func() {
 			defer wg.Done()
 
--- 2_1_rate_limiter/server/main.go
+++ 2_1_rate_limiter/server/main.go
@@ -6,19 +6,20 @@ import (
 	"time"
 )
 
+var url = "localhost:8080"
+var limit = 10
+
 // RateLimiter для управления одновременными запросами
 type RateLimiter struct {
 	semaphore chan struct{}
 }
 
-// NewRateLimiter создает новый RateLimiter с заданным количеством одновременных запросов
 func NewRateLimiter(maxConcurrent int) *RateLimiter {
 	return &RateLimiter{
 		semaphore: make(chan struct{}, maxConcurrent),
 	}
 }
 
-// Allow позволяет запросу пройти, если это возможно
 func (rl *RateLimiter) Allow() bool {
 	select {
 	case rl.semaphore <- struct{}{}:
@@ -28,33 +29,33 @@ func (rl *RateLimiter) Allow() bool {
 	}
 }
 
-// Done освобождает место для следующего запроса
 func (rl *RateLimiter) Done() {
 	<-rl.semaphore
 }
 
-// Обработчик для HTTP-запросов
 func requestHandler(w http.ResponseWriter, r *http.Request, limiter *RateLimiter) {
 	if limiter.Allow() {
-		defer limiter.Done() // Освобождаем место по завершении
+		defer limiter.Done()
 
-		fmt.Fprintf(w, "Обработка запроса: %s\n", r.URL.Path)
-		time.Sleep(1 * time.Second) // Симуляция обработки
+		_, err := fmt.Fprintf(w, "Обработка запроса: %s\n", r.URL.Path)
+		if err != nil {
+			return
+		}
+		time.Sleep(1500 * time.Millisecond) // Симуляция обработки
 	} else {
 		http.Error(w, "Слишком много одновременно обрабатываемых запросов", http.StatusTooManyRequests)
 	}
 }
 
 func main() {
-	// Создаем rate limiter, позволяющий максимум 3 одновременных запроса
-	limiter := NewRateLimiter(3)
+	limiter := NewRateLimiter(limit)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestHandler(w, r, limiter)
 	})
 
-	fmt.Println("Сервер запущен на http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Сервер запущен на http://", url)
+	if err := http.ListenAndServe(url, nil); err != nil {
 		fmt.Println("Ошибка при запуске сервера:", err)
 	}
 }
